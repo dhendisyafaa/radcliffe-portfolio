c47a1ee6ee93e63ef970638f37b3e039f434e239
Dhendi Syafa Athallah Putra <dhendisyafa@gmail.com>
2024-12-22 21:23:38 +0700

    feat: remove unused component

--- src/pages/api/resolvers/linkedinResolver.js
+++
@@ -1,8 +0,0 @@
-import { useMutation, useQueryClient } from "@tanstack/react-query";
-import useQueryNoRefecth from "../hooks/useQueryNoRefetch";
-import { getDataProfile } from "../services/linkedinApi";
-
-const useDataProfile = () =>
-  useQueryNoRefecth(["me"], async () => await getDataProfile());
-
-export { useDataProfile };
